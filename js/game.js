330a282952c7b15c667ff1aa92438a5283ea9efa
Ludovic Coues <gkr@jellycopter.net>
2016-10-17 22:38:08 +0200

    Add a grid, car, lot of tree

--- js/game.js
+++ js/game.js
@@ -18,9 +18,9 @@ function init()
 {
 	// 1366 x 768
 	var ratio = window.innerWidth / window.innerHeight
-	camera = new THREE.PerspectiveCamera(60, ratio, 1000, 1000000)
+	camera = new THREE.PerspectiveCamera(60, ratio, 1000, 2000000)
 	camera.position.y = 500000
-	camera.position.z = 550000
+	camera.position.z = 750000
 	camera.lookAt({x:0, y:0, z:0})
 	scene = new THREE.Scene()
 	renderer = new THREE.WebGLRenderer({antialias: false})
@@ -38,32 +38,58 @@ function init()
 	var loader = new THREE.OBJLoader()
 	loader.setMaterials({create: function() { return pine_mat }})
 	loader.load("assets/pine1.obj", function(pine) {
-		var circle_geo = new THREE.CircleGeometry(350000, 16)
+		var circle_geo = new THREE.CircleGeometry(600000, 32)
 		circle_geo.rotateX(Math.TAU * .75)
 		var circle_mat = new THREE.MeshBasicMaterial({color: 0x00ff00})
 		var circle = new THREE.Mesh(circle_geo, circle_mat)
 		scene.add(circle)
-		console.log(circle.geometry)
-		for (i=1; i<circle.geometry.vertices.length-1; ++i) {
-			console.log(circle.geometry.vertices[i])
+		var circle_geo = new THREE.CircleGeometry(550000, 32)
+		circle_geo.rotateX(Math.TAU * .75)
+		circle_geo.rotateY(Math.TAU * Math.random())
+		for (i=1; i<circle_geo.vertices.length-1; ++i) {
 			p = pine.clone()
 			p.rotation.y = Math.TAU * Math.random()
-			p.position.add(circle.geometry.vertices[i])
+			p.position.add(circle_geo.vertices[i])
+			scene.add(p)
+		}
+		var circle_geo = new THREE.CircleGeometry(500000, 20)
+		circle_geo.rotateX(Math.TAU * .75)
+		circle_geo.rotateY(Math.TAU * Math.random())
+		for (i=1; i<circle_geo.vertices.length-1; ++i) {
+			p = pine.clone()
+			p.rotation.y = Math.TAU * Math.random()
+			p.position.add(circle_geo.vertices[i])
+			scene.add(p)
+		}
+		var circle_geo = new THREE.CircleGeometry(450000, 28)
+		circle_geo.rotateX(Math.TAU * .75)
+		circle_geo.rotateY(Math.TAU * Math.random())
+		for (i=1; i<circle_geo.vertices.length-1; ++i) {
+			p = pine.clone()
+			p.rotation.y = Math.TAU * Math.random()
+			p.position.add(circle_geo.vertices[i])
 			scene.add(p)
 		}
 	})
-	var car_txt = new THREE.TextureLoader().load("assets/car_yellow.png")
-	car_txt.magFilter = THREE.NearestFilter
-	var car_mat = new THREE.MeshBasicMaterial({
-		name: "car_base_color", map: car_txt
-	})
+	var wood_mat = new THREE.MeshBasicMaterial({color: 0x773027})
+	var velvet_mat = new THREE.MeshBasicMaterial({color: 0x185F0C})
 	var loader = new THREE.OBJLoader()
-	loader.setMaterials({create: function() { return car_mat }})
-	loader.load("assets/car_yellow.obj", function(car) {
+	loader.setMaterials({create: function(txt) {  switch(txt) {
+		case "wood":	return wood_mat
+		case "velvet":	return velvet_mat
+	}}})
+	loader.load("assets/grid.obj", function(grid) {
+		box = new THREE.Box3().setFromObject(grid)
+		base = box.size()
+		grid.scale.x = 50000
+		grid.scale.y = 50000
+		grid.scale.z = 50000
+		scene.add(grid)
+	})
+	loadCar("car_yellow", function(car) {
 		objs.car = car
-		car.scale.x = 3
-		car.scale.y = 3
-		car.scale.z = 3
+		car.position.y = 21000
+		car.position.z = -30000
 		scene.add(car)
 	})
 }
@@ -71,8 +97,7 @@ function init()
 function update()
 {
 	requestAnimationFrame(update)
-	if (objs.car)
-		objs.car.rotation.y -= 0.01
+	scene.rotation.y -= 0.005
 	renderer.render(scene, camera)
 }
 
@@ -84,10 +109,28 @@ function onMouseMove(event)
 
 function onResize()
 {
-	console.log(window.innerWidth, window.innerHeight)
 	windowHalfX = window.innerWidth / 2
 	windowHalfY = window.innerHeight / 2
 	camera.aspect = window.innerWidth / window.innerHeight
 	camera.updateProjectionMatrix()
 	renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function loadCar(car, fn)
+{
+	var car_txt = new THREE.TextureLoader().load("assets/"+car+".png")
+	car_txt.magFilter = THREE.NearestFilter
+	var car_mat = new THREE.MeshBasicMaterial({
+		name: "car_base_color", map: car_txt
+	})
+	var loader = new THREE.OBJLoader()
+	loader.setMaterials({create: function() { return car_mat }})
+	loader.load("assets/"+car+".obj", function(car) {
+		box = new THREE.Box3().setFromObject(car)
+		base = box.size()
+		car.scale.x = 100000 / base.x
+		car.scale.y = 50000 / base.y
+		car.scale.z = 50000 / base.z
+		fn(car)
+	})
 }
